6989a4497ce98f4325b40f3b94a8ab4698a937e6
Jeric James Viernes <jericviernes06@gmail.com>
2023-10-18 06:33:07 +0800

    added 9 color shades of wood

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./public/*.php"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'wood-100':'#DDBDA2',
+        'wood-200':'#C4A389',
+        'wood-300':'#AA8A6F',
+        'wood-400':'#957861',
+        'wood-500':'#806753',
+        'wood-600':'#6B5646',
+        'wood-700':'#554538',
+        'wood-800':'#40342A',
+        'wood-900':'#2B221C',
+      }
+    },
   },
   plugins: [],
 }
